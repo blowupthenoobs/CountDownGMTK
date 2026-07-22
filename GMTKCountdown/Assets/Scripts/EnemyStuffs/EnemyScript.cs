feb008372c75417dbdde181d2c0c47a99d97b809
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2026-07-22 16:35:13 -0400

    Maybe Enemy can see player

--- GMTKCountdown/Assets/Scripts/EnemyStuffs/EnemyScript.cs
+++ GMTKCountdown/Assets/Scripts/EnemyStuffs/EnemyScript.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public abstract class EnemyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject target;
+    private 
+
     void Start()
     {
         
@@ -14,5 +16,30 @@ public abstract class EnemyScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    protected bool CanSeePlayer()
+    {
+        if(target == null)
+            return false;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.position - target.transform.position);
+
+        if(hit)
+            return true;
+
+        return false;
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+            target = collision.gameObject;
+    }
+
+    protected void OnTriggerExit2D(Collider2D collision)
+    {
+        if(target == collision.gameObject)
+            target = null;
     }
 }
