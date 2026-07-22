9b635fed2d0b6d95764dc04eb3cdb0525473f542
JustAMuffinDev-offical <amuffindev@gmail.com>
2026-07-22 15:15:43 -0600

    Camera
    paparazzi

--- GMTKCountdown/Assets/Scripts/CameraFollower.cs
+++ GMTKCountdown/Assets/Scripts/CameraFollower.cs
@@ -9,15 +9,37 @@ public class CameraFollower : MonoBehaviour
     private Vector3 playerPos;
 
     private Vector3 newPos;
-    
+    private float halfCamHeight;
+    private float halfCamWidth;
+
+    private Camera cam;
+    private Vector3 oldPos;
+
     public float followSpeed;
     void Awake()
     {
         Player = GameObject.FindWithTag("Player");
+
+        cam = gameObject.GetComponent<Camera>();
+        halfCamHeight = cam.orthographicSize;
+        halfCamWidth = halfCamHeight * cam.aspect;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("NodeX"))
+        {
+            transform.position = new Vector3(oldPos.x, transform.position.y, transform.position.z);
+        }
+        if(other.CompareTag("NodeY"))
+        {
+            transform.position = new Vector3(transform.position.x, oldPos.y, transform.position.z);
+        }
     }
 
     void FixedUpdate()
     {
+        oldPos = transform.position;
         playerPos = Player.gameObject.transform.position;
 
         newPos = Vector3.Lerp(transform.position, playerPos, followSpeed);
