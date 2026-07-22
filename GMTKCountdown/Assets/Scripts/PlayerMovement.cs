9f6e3a6bb1a2a86ef7f776a8e4de8d9c4c803e2f
ABananaDev <abananadev@gmail.com>
2026-07-22 15:16:54 -0700

    Weapons
    Still in progress.

--- GMTKCountdown/Assets/Scripts/PlayerMovement.cs
+++ GMTKCountdown/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,12 +10,20 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 normalizedInput;
 
     private Rigidbody2D rb;
-    
+
     public float speed;
+
+    [SerializeField] GameObject[] weapons;
+    [SerializeField] Transform weaponsSpawn;
     
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();   
+    }
+
+    void Update()
+    {
+
     }
 
     void FixedUpdate()
@@ -25,5 +34,29 @@ public class PlayerMovement : MonoBehaviour
         normalizedInput = new Vector2(hInput, vInput).normalized;
         
         rb.MovePosition(rb.position + normalizedInput * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Sword"))
+        {
+            Instantiate(weapons[0], weaponsSpawn.transform.position, weaponsSpawn.transform.rotation, weaponsSpawn.transform);
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("Revolver"))
+        {
+            Instantiate(weapons[1], weaponsSpawn.transform.position, weaponsSpawn.transform.rotation, weaponsSpawn.transform);
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("Shotgun"))
+        {
+            Instantiate(weapons[2], weaponsSpawn.transform.position, weaponsSpawn.transform.rotation, weaponsSpawn.transform);
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("Sniper"))
+        {
+            Instantiate(weapons[3], weaponsSpawn.transform.position, weaponsSpawn.transform.rotation, weaponsSpawn.transform);
+            Destroy(other.gameObject);
+        }
     }
 }
