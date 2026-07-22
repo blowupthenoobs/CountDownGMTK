e3367dc596a613f6acc776a0a8e1eb1f8cd16dcb
JustAMuffinDev-offical <amuffindev@gmail.com>
2026-07-22 13:28:49 -0600

    Player again
    better this time

---
+++ GMTKCountdown/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+
+    private GameObject Player;
+    
+    void Start()
+    {
+        //Player = FindWithTag("Player");
+        Debug.Log(Player);
+    }
+
+    void Update()
+    {
+        
+    }
+}
