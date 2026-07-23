56aef22ae41db7a69be02064dc05e6bb4fd6cd57
ABananaDev <abananadev@gmail.com>
2026-07-23 13:27:42 -0700

    Melee System

--- GMTKCountdown/Assets/Scripts/Player/PlayerWeapon.cs
+++ GMTKCountdown/Assets/Scripts/Player/PlayerWeapon.cs
@@ -20,11 +20,24 @@ public class PlayerWeapon : MonoBehaviour
 
     public BulletSpawnPoint bulletSpawnPoint;
 
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] Vector2 raycastSize;
+
+    public bool isMeleeWeapon;
+    public bool isInRange;
+
     void Update()
     {
         SwitchIDValues();
         FireRate();
         Rotate();
+
+        isInRange = Physics2D.OverlapBox(transform.position, raycastSize, 0f, enemyLayer);
+
+        if((Input.GetMouseButtonDown(0)) && isInRange && isMeleeWeapon)
+        {
+            Debug.Log("Did damage to enemy!");
+        }
     }
 
     void FireRate()
@@ -61,10 +74,12 @@ public class PlayerWeapon : MonoBehaviour
         if(ID == 0)
         {
             spriteRenderer.sprite = null;
+            isMeleeWeapon = true;
         }
         if(ID == 1)
         {
             spriteRenderer.sprite = weaponSprites[0];
+            isMeleeWeapon = true;
             //tbadded
         }
         if(ID == 2)
@@ -72,6 +87,7 @@ public class PlayerWeapon : MonoBehaviour
             bulletSpawnPoint.GoToPos(-0.315f, 1.426f);
             fireRate = 0.5f;
             spriteRenderer.sprite = weaponSprites[1];
+            isMeleeWeapon = false;
 
             if(requestedToShoot)
             {
@@ -83,8 +99,9 @@ public class PlayerWeapon : MonoBehaviour
             bulletSpawnPoint.GoToPos(-0.348f, 1.63f);
             fireRate = 0.85f;
             spriteRenderer.sprite = weaponSprites[2];
+            isMeleeWeapon = false;
 
-            if(requestedToShoot)
+            if (requestedToShoot)
             {
                 Instantiate(bullets[1], bulletSpawn.transform.position, bulletSpawn.transform.rotation);
             }
@@ -94,11 +111,18 @@ public class PlayerWeapon : MonoBehaviour
             bulletSpawnPoint.GoToPos(-0.16f, 1.89f);
             fireRate = 1f;
             spriteRenderer.sprite = weaponSprites[3];
+            isMeleeWeapon = false;
 
             if (requestedToShoot)
             {
                 Instantiate(bullets[2], bulletSpawn.transform.position, bulletSpawn.transform.rotation);
             }
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, raycastSize);
     }
 }
